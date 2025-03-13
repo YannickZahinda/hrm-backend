63d6b21cea4bf0bdb2062d98371480987d61a97f
YannickZahinda <ymulikuza@gmail.com>
2025-03-13 14:24:52 +0200

    add employee service basic logic

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Employee } from './employee.entity';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
 
 @Injectable()
 export class EmployeeService {
@@ -11,6 +12,15 @@ export class EmployeeService {
 
     async findAll(){
         return await this.employeeRepo.find({relations: ['documents']});
+    }
+
+    async create(createEmployeeDto: CreateEmployeeDto) {
+        const employee = this.employeeRepo.create(createEmployeeDto);
+        return await this.employeeRepo.save(employee);
+    }
+
+    async remove(id: number){
+        return await this.employeeRepo.delete(id);
     }
 
 }
