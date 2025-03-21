2bc1e0018fe57f9121ca3f9e71230ad1828d60e8
YannickZahinda <ymulikuza@gmail.com>
2025-03-21 12:38:08 +0200

    add frontend and modify config to include frontend

--- electron.js
+++ electron.js
@@ -1,8 +1,27 @@
 const { app, BrowserWindow } = require('electron');
-// const path = require('path');
-// const { exec } = require('child_process');
+const path = require('path');
+const { exec } = require('child_process');
+const { stderr, stdout } = require('process');
 
 let mainWindow;
+
+if(process.env.NODE_ENV === "development") {
+  exec("npm run start", (err, stdout, stderr) => {
+    if(err) {
+      console.error(`Error starting backend: ${err}`);
+      return;
+    }
+    console.log(`Backend output: ${stdout}`);
+  });
+
+  exec("cd frontend && npm run dev", (err, stdout, stderr) => {
+    if(err) {
+      console.error(`Error starting the frontend: ${err}`);
+      return;
+    }
+    console.log(`Fronted Out: ${stdout}`)
+  })
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -12,8 +31,13 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
+  if(process.env.NODE_ENV === 'development'){
+    mainWindow.loadURL('http://localhost:5173');
+    // mainWindow.loadURL('http://localhost:3000');
+  }else {
+    mainWindow.loadFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+  }
 
-  mainWindow.loadURL('http://localhost:3000');
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
