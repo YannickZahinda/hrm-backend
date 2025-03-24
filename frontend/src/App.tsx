6e1f43090612cd49b2f4fe33b8a3a713a4d77a68
YannickZahinda <ymulikuza@gmail.com>
2025-03-24 14:56:57 +0200

    modify app to include sidebar'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,39 @@
 import './App.css'
+import { useState } from 'react'
 import { Dashboard } from './components/dashboard'
+import { Sidebar } from './components/sidebar'
+import { EmployeeList } from './components/employee-list'
+import { LeaveManagement } from './components/leave-management'
+import { DocumentManagement } from './components/document-management'
+import { Settings } from './components/settings'
 
 function App() {
+  const [activeView, setActiveView] = useState("dashboard");
+
+  const renderContent = () => {
+    switch (activeView) {
+      case "dashboard":
+        return <Dashboard />
+      case "employees": 
+        return <EmployeeList />
+      case "leaves":
+        return <LeaveManagement />
+      case "documents": 
+        return <DocumentManagement />
+      case "settings":
+        return <Settings />
+      default: 
+        return <Dashboard />
+    }
+  }
 
   return (
     <>
-      <div>
-        <Dashboard />
+      <div className='flex h-screen bg-background'>
+       <Sidebar activeView={activeView} setActiveView={setActiveView} />
+       <main className='flex-1 overflow-auto p-6'>
+        {renderContent()}
+       </main>
       </div>
     </>
   )
